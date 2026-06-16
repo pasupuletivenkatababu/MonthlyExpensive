50878974fd542582d019d2ef7c14104645d453e9
Pasupuletivenkatababu <venkatababupasupuleti@gmail.com>
2026-06-16 22:16:07 +0530

    Rename component files to match imports

--- src/App.jsx
+++ src/App.jsx
@@ -2,22 +2,22 @@ import React from "react";
 import "./index.css";
 import { BudgetProvider } from "./context/BudgetContext";
 import Header from "./components/Header";
-import BudgetInput from "./components/BudgetInput";
+import Budgetinput from "./components/Budgetinput";
 import BudgetSummary from "./components/BudgetSummary";
 import AddExpenseForm from "./components/AddExpenseForm";
 import ExpenseChart from "./components/ExpenseChart";
-import ExpenseList from "./components/ExpenseList";
+import Expenselist from "./components/Expenselist";
 
 export default function App() {
   return (
     <BudgetProvider>
       <div className="ledger-page">
         <Header />
-        <BudgetInput />
+        <Budgetinput />
         <BudgetSummary />
         <AddExpenseForm />
         <ExpenseChart />
-        <ExpenseList />
+        <Expenselist />
       </div>
     </BudgetProvider>
   );
